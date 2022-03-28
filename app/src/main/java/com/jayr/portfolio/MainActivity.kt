a66c2fc386fc62e03286b7b5a4300b467b5ffdd0
josephridge <josephridge18@gmail.com>
2022-03-28 11:57:24 +0300

    Design of the xtra large screen layout.

--- app/src/main/java/com/jayr/portfolio/MainActivity.kt
+++ app/src/main/java/com/jayr/portfolio/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.jayr.portfolio
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+//import kotlin.androidx.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        Making the corner radius of an image rounded
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius= 15f
+        val appLogo = findViewById<ImageView>(R.id.logo)
+        appLogo.setImageDrawable(rounded)
+
     }
 }
